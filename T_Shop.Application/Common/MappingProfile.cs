03f81b6c2e9b376ff4193f0d73d36f0afd4b9270
Ashfaq Ahamed <mnashfaqahamed@gmail.com>
2024-01-27 21:08:22 +0530

    DTO and Auto Mapper

---
+++ T_Shop.Application/Common/MappingProfile.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using T_Shop.Application.DTO;
+using T_Shop.Domain.Models;
+
+namespace T_Shop.Application.Common
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Category, CreateCategoryDto>().ReverseMap();
+            CreateMap<Category, UpdateCategoryDto>().ReverseMap();
+            CreateMap<Category, CategoryDto>().ReverseMap();
+        }
+    }
+}
